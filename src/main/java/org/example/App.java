a00676efda600efd39ac5d43cd946abfeee4d6a9
Hailey Desktop <haileytapia@knights.ucf.edu>
2021-09-12 00:03:17 -0400

    Added variables.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,22 +1,23 @@
 /*
  *  UCF COP3330 Fall 2021 Assignment 1 Solution
- *  Copyright 2021 tapia hailey
+ *  Copyright 2021 Hailey Tapia
  */
 
 package org.example;
 
 import java.util.Scanner;
 
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        String name;
+        String name, output;
         Scanner input = new Scanner(System.in);
 
         System.out.print("What is your name? ");
         name = input.nextLine();
+        output = "Hello, " + name + ", nice to meet you!";
 
-        System.out.print("Hello, " + name + ", nice to meet you!");
+        System.out.print(output);
     }
 }

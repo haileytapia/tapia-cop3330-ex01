3a853e9ed64b3497905753496c9165b851628f87
Hailey Desktop <haileytapia@knights.ucf.edu>
2021-09-11 22:24:23 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 tapia hailey
+ */
+
+package org.example;
+
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String name;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is your name? ");
+        name = input.nextLine();
+
+        System.out.print("Hello, " + name + ", nice to meet you!");
+    }
+}
